76c33fd1398d095f56942eaa0a0ffd76d07436e0
Bianca Fu <0509biancafu@gmail.com>
2022-11-18 23:52:05 +0000

    W1D5 recursive sum

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  // 3 4 5 6 7
+  // 3 + sum(4,7)
+  // 3 + 4 + sum(5,7)
+  if (fromN === toN) { //base case
+    return fromN;
+  }
+  return fromN + sum(fromN + 1,toN);
 }
 
 module.exports = sum;
